02bada8b2180770de3e950448f8b401ee0507ebf
StephenTerror <2684109034@qq.com>
2021-07-02 19:24:59 +0800

    init commit

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -16,9 +16,9 @@ from .other_models import DLA, SimpleDLA, DPN26, DPN92, EfficientNetB0, GoogLeNe
 from .other_models import PNASNetA, PNASNetB, MobileNet, MobileNetV2
 from .other_models import PreActResNet18, PreActResNet34, PreActResNet50, PreActResNet152, PreActResNet101
 from .other_models import RegNetX_200MF, RegNetX_400MF, SENet18
-from .other_models import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
-from .other_models import ResNeXt29_2x64d, ResNeXt29_4x64d, ResNeXt29_8x64d, ResNeXt29_32x4d
-from .other_models import ShuffleNetG2, ShuffleNetG3, ShuffleNetV2, ResNet101, ResNet152
-from .other_models import VGG11, VGG13, VGG16, VGG19
+# from .other_models import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
+# from .other_models import ResNeXt29_2x64d, ResNeXt29_4x64d, ResNeXt29_8x64d, ResNeXt29_32x4d
+# from .other_models import ShuffleNetG2, ShuffleNetG3, ShuffleNetV2, ResNet101, ResNet152
+# from .other_models import VGG11, VGG13, VGG16, VGG19
 from .transformer import *
 # ------------------------------------------------ classify ------------------------------------------------ #
