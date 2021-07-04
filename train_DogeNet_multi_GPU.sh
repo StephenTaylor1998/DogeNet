fa7abf43c71a29876d06986a2124b95dbafdd8ff
StephenTerror <2684109034@qq.com>
2021-07-04 10:39:22 +0800

    update

--- train_DogeNet_multi_GPU.sh
+++ train_DogeNet_multi_GPU.sh
@@ -2,6 +2,7 @@
 #/home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/
 #/home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/svhn/
 
+echo "[INFO] Starting..."
 
 python train_imagenet.py -d image_folder -a bot_net50_l1 -b 512 -j 2 -c 10 --epoch 1 --in-shape 3 224 224 \
 --data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > bot_net50_l1.log
@@ -14,3 +15,5 @@ python train_imagenet.py -d image_folder -a doge_net26 -b 512 -j 2 -c 10 --epoch
 
 python train_imagenet.py -d image_folder -a doge_net50 -b 512 -j 2 -c 10 --epoch 2 --in-shape 3 224 224 \
 --data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > doge_net50.log
+
+echo "[INFO] Done."
