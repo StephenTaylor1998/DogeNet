9e44f2cbcdaaf8c7f07ef487d327315dd209c1a5
StephenTerror <2684109034@qq.com>
2021-07-04 21:47:08 +0800

    update

--- train_DogeNet_multi_GPU.sh
+++ train_DogeNet_multi_GPU.sh
@@ -4,16 +4,19 @@
 
 echo "[INFO] Starting..."
 
-python train_imagenet.py -d image_folder -a bot_net50_l1 -b 512 -j 2 -c 10 --epoch 300 --in-shape 3 224 224 \
---data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > bot_net50_l1.log
+#python train_imagenet.py -d image_folder -a bot_net50_l1 -b 512 -j 2 -c 10 --epoch 300 --in-shape 3 224 224 \
+#--data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > bot_net50_l1.log
 
-python train_imagenet.py -d image_folder -a bot_net50_l2 -b 512 -j 2 -c 10 --epoch 300 --in-shape 3 224 224 \
---data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > bot_net50_l2.log
+#python train_imagenet.py -d image_folder -a bot_net50_l2 -b 512 -j 2 -c 10 --epoch 300 --in-shape 3 224 224 \
+#--data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > bot_net50_l2.log
 
-python train_imagenet.py -d image_folder -a doge_net26 -b 512 -j 2 -c 10 --epoch 300 --in-shape 3 224 224 \
---data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > doge_net26.log
+#python train_imagenet.py -d image_folder -a doge_net26 -b 512 -j 2 -c 10 --epoch 300 --in-shape 3 224 224 \
+#--data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > doge_net26.log
 
-python train_imagenet.py -d image_folder -a doge_net50 -b 512 -j 2 -c 10 --epoch 300 --in-shape 3 224 224 \
---data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > doge_net50.log
+#python train_imagenet.py -d image_folder -a doge_net50 -b 512 -j 2 -c 10 --epoch 300 --in-shape 3 224 224 \
+#--data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > doge_net50.log
+
+python train_imagenet.py -d image_folder -a doge_net_2x1x3x2 -b 512 -j 2 -c 10 --epoch 300 --in-shape 3 224 224 \
+--data-path /home/aistudio/Desktop/remote/high-resolution-capsule/data/dataset/sub_imagenet/ > doge_net_2x1x3x2.log
 
 echo "[INFO] Done."
